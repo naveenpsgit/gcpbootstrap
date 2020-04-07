6b8435c6941aa0d9aa30ea08816e069e3c491d01
Naveen PS <37842303+naveenpsgit@users.noreply.github.com>
2020-04-07 13:33:46 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('Hello World from GCE!'); 
+   res.send('Hello World from GCE! update 1'); 
 });
 
 app.listen(8080, function () {
